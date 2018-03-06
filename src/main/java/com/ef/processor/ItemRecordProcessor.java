546dd961b967786a536c8df69003500737a26f17
Julian <madknight@gmail.com>
2018-03-05 21:42:09 -0500

    Including parameters into Processor

--- src/main/java/com/ef/processor/ItemRecordProcessor.java
+++ src/main/java/com/ef/processor/ItemRecordProcessor.java
@@ -3,14 +3,18 @@ package com.ef.processor;
 import java.util.Map;
 
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.beans.factory.annotation.Value;
 
 public class ItemRecordProcessor implements ItemProcessor<Map<String, Object>, Map<String, Object>> {
 
+    @Value("${ip}")
+    String ip_filter;
+    
     @Override
     public Map<String, Object> process(Map<String, Object> item) throws Exception {
         if(item.containsKey("column2") && item.get("column2") != null){
             String ip = (String)item.get("column2");
-            if("192.168.234.82".equals(ip.trim())){
+            if(ip_filter.equals(ip.trim())){
                 return item;
             }
         }
